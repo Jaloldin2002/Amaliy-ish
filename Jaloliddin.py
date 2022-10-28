aced90d5bb43299e7804cfc770a0073cbed8ad93
Jaloldin2002 <116884240+Jaloldin2002@users.noreply.github.com>
2022-10-28 15:46:36 +0500

    Add files via upload

---
+++ Jaloliddin.py
@@ -0,0 +1,14 @@
+# 1=amaliy
+a=int(input(' a sonni kiriting='))
+b=int(input(' b sonni kiriting='))
+s=a+b
+d=a-b
+e=a*b
+f=a/b
+print(f'yigindi={s} , ayirma={d} , kopaytma={e} , bolinma={f}')
+
+# 1.1-amaliy
+a=int(input(' a sonni kiriting='))
+b=int(input(' b sonni kiriting='))
+d=(a+b)/2
+print(f' orta arifmetik={d}')
